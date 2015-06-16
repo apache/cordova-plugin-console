b4899565bed6f9267856d57f48e6b2672c55de56
Jesse MacFadyen <purplecabbage@gmail.com>
2015-06-16 16:00:23 -0700

    clean up tests, info is below log level so it does not exist by default.

--- tests/tests.js
+++ tests/tests.js
@@ -19,31 +19,25 @@
  *
 */
 
-exports = {
-    defineAutoTests:function () {
-      describe("Console", function () {
-        it("console.spec.1 should exist", function() {
-            expect(window.console).toBeDefined();
-        });
+exports.defineAutoTests = function () {
+  describe("Console", function () {
+    it("console.spec.1 should exist", function() {
+        expect(window.console).toBeDefined();
+    });
+    
+    it("console.spec.2 has required methods log|warn|error", function(){
+        expect(window.console.log).toBeDefined();
+        expect(typeof window.console.log).toBe('function');
         
-        it("console.spec.2 has required methods",function(){
-            expect(window.console.log).toBeDefined();
-            expect(typeof window.console.log).toBe('function');
-            
-            expect(window.console.warn).toBeDefined();
-            expect(typeof window.console.warn).toBe('function');
-            
-            expect(window.console.error).toBeDefined();
-            expect(typeof window.console.error).toBe('function');
-            
-            expect(window.console.info).toBeDefined();
-            expect(typeof window.console.info).toBe('function');
-        });
+        expect(window.console.warn).toBeDefined();
+        expect(typeof window.console.warn).toBe('function');
         
-      });
-    },
-
-    defineManualTests:function (contentEl, createActionButton) {
-      
-    }
+        expect(window.console.error).toBeDefined();
+        expect(typeof window.console.error).toBe('function');                   
+        
+    });
+    
+  });
 };
+
+exports.defineManualTests = function (contentEl, createActionButton) {};
