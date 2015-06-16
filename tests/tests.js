e888a5b2580c1adf5a50d28692179d19d93b9ef0
Jesse MacFadyen <purplecabbage@gmail.com>
2015-06-16 14:05:17 -0700

    add a couple tests

--- tests/tests.js
+++ tests/tests.js
@@ -19,13 +19,31 @@
  *
 */
 
-exports.defineAutoTests = function () {
+exports = {
+    defineAutoTests:function () {
       describe("Console", function () {
         it("console.spec.1 should exist", function() {
-          expect(window.console).toBeDefined();
+            expect(window.console).toBeDefined();
+        });
+        
+        it("console.spec.2 has required methods",function(){
+            expect(window.console.log).toBeDefined();
+            expect(typeof window.console.log).toBe('function');
+            
+            expect(window.console.warn).toBeDefined();
+            expect(typeof window.console.warn).toBe('function');
+            
+            expect(window.console.error).toBeDefined();
+            expect(typeof window.console.error).toBe('function');
+            
+            expect(window.console.info).toBeDefined();
+            expect(typeof window.console.info).toBe('function');
+        });
+        
       });
-};
+    },
 
-exports.defineManualTests = function (contentEl, createActionButton) {
-  
+    defineManualTests:function (contentEl, createActionButton) {
+      
+    }
 };
